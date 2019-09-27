6e654ac65e78ff555a044a62a232e230143047a2
YuriNa <nyryn0945@gmail.com>
2019-09-27 18:13:41 +0900

    todo list format

--- src/components/content/TodoList.js
+++ src/components/content/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { Box, List, ListItem, ListItemIcon, ListItemText, Checkbox, Typography, Chip } from '@material-ui/core'
+import { format } from 'date-fns'
 // import StarBorderIcon from '@material-ui/icons/StarBorder'
 
 const useStyles = makeStyles(theme => ({
@@ -17,7 +18,7 @@ const useStyles = makeStyles(theme => ({
 	},
 	itemTypo: {
 		flexGrow: 1,
-		color: props => (props.checked ? theme.palette.text.hint : theme.palette.text.primary),
+		color: props => (props.completed ? theme.palette.text.hint : theme.palette.text.primary),
 	},
 	itemChip: {
 		marginLeft: theme.spacing(1),
@@ -28,41 +29,52 @@ const useStyles = makeStyles(theme => ({
 	},
 }))
 
-function TodoItem({ text, time, selected, checked }) {
-	const classes = useStyles({ checked })
+function TodoItem({ item }) {
+	const classes = useStyles({ completed: item.completed })
 
 	return (
-		<ListItem button selected={selected} classes={{ root: classes.listItem, selected: classes.selectedItem }}>
+		<ListItem button selected={!!item.selected} classes={{ root: classes.listItem, selected: classes.selectedItem }}>
 			<ListItemIcon classes={{ root: classes.listItemIcon }}>
-				<Checkbox checked={checked} color="primary" edge="start" />
+				<Checkbox checked={item.completed} color="primary" edge="start" />
 			</ListItemIcon>
 			<ListItemText
 				disableTypography
+				classes={{ root: classes.listItemText }}
 				primary={
 					<>
-						<Typography classes={{ root: classes.itemTypo }}>{text}</Typography>
-						{time && <Chip label={time} size="small" classes={{ root: classes.itemChip }} />}
+						<Typography classes={{ root: classes.itemTypo }}>{item.content}</Typography>
+						{item.dueDate && <Chip label={format(item.dueDate, 'M월 d일')} size="small" classes={{ root: classes.itemChip }} />}
 					</>
 				}
-				classes={{ root: classes.listItemText }}
 			/>
 		</ListItem>
 	)
 }
 
 function TodoList() {
+	const [selected, setSelected] = React.useState(-1)
+
+	const items = [
+		{ id: 0, content: '할일입니다', completed: false, list: 1, created: new Date(), updated: new Date(), dueDate: new Date() },
+		{
+			id: 1,
+			content:
+				'아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다',
+			completed: false,
+			list: 1,
+			created: new Date(),
+			updated: new Date(),
+			dueDate: new Date(),
+		},
+		{ id: 2, content: '할일입니다', completed: false, list: 1, created: new Date(), updated: new Date(), dueDate: null },
+		{ id: 3, content: '할일입니다', completed: true, list: 1, created: new Date(), updated: new Date(), dueDate: null },
+	]
+
 	return (
-		<Box px={3}>
+		<Box px={3} pb={3}>
 			<List disablePadding>
-				<TodoItem text="할일입니다" time="오후 4:00" />
-				<TodoItem
-					text="아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다 아주 긴 할 일입니다"
-					time="7월 3일"
-				/>
-				<TodoItem text="할일입니다" selected />
-				<TodoItem text="할일입니다" checked />
-				{Array.from({ length: 20 }).map((v, k) => (
-					<TodoItem text={`${k + 1}번째 할일입니다`} key={`test-${k}`} />
+				{items.map(item => (
+					<TodoItem key={`todo-${item.id}`} item={item} />
 				))}
 			</List>
 		</Box>
